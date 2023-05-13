4a34fb50f33c73be79bdacc4d5df5bc9eca61c04
vbarter <yzcaijunjie@gmail.com>
2023-05-13 12:25:42 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,17 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.host = "www.discord.com";
-    return fetch(url, {
+    url.host = "discord.com";
+    url.pathname = "/api/v9/interactions"; // Discord API 的路径
+    const response = await fetch(url, {
       headers: request.headers,
       method: request.method,
       body: request.body
-     // redirect: 'follow'
+    });
+    return new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: response.headers
     });
   }
 }
