a6a154f4dd949d28fd02eea9ff8d73da9364e4c8
vbarter <yzcaijunjie@gmail.com>
2023-05-13 12:54:22 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,10 +1,10 @@
 export default { 
   async fetch(request, env) {
-    const targetUrl = 'www.discord.com'; // 指定目标 URL
+    const targetUrl = 'https://www.discord.com'; // 指定目标 URL
     const url = new URL(request.url);
     let newRequest;
 
-    // 如果请求路径以 / 开头，则转发到目标 URL
+    // 如果请求路径以 /api/v9/interactions 开头，则转发到目标 URL
     if (url.pathname.startsWith('/api/v9/interactions')) {
       url.hostname = targetUrl;
       newRequest = new Request(url, {
@@ -14,13 +14,13 @@ export default {
       });
       return fetch(newRequest);
     } else {
-      return fetch(url, {
-        headers: request.headers,
-        method: request.method,
-        body: request.body
-        // redirect: 'follow'
+      newRequest = new Request(url, {
+        method: request.method, // 指定使用原始请求的方法
+        body: request.body, // 使用原始请求的主体
+        headers: request.headers, // 使用原始请求的所有头部
+        redirect: 'follow', // 跟随重定向
       });
+      return fetch(newRequest);
     }
   },
-  
 };
