83d393cb5356cea70774c8b40320cd8849f3954b
vbarter <yzcaijunjie@gmail.com>
2023-05-13 13:24:07 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,15 +7,15 @@ async function handleRequest(request) {
   const url = new URL(request.url);
   let newRequest;
 
-  // 如果请求路径以 /api/v9/interactions 开头，则转发到目标 URL
-  if (url.pathname.startsWith('/api/v9/interactions')) {
-    // url.hostname = targetUrl;
+  // 如果请求路径以 /api/v9/interactions 开头，并且是 POST 请求，则转发到目标 URL
+  if (url.pathname.startsWith('/api/v9/interactions') && request.method === 'POST') {
+    url.hostname = targetUrl;
     newRequest = new Request(url, {
       method: 'POST', // 指定使用 POST 方法
       body: request.body, // 将原始请求的主体作为新请求的主体
-      headers: request.headers, // 使用原始请求的所有头
+      headers: request.headers, // 使用原始请求的所有头部
     });
-    return fetch(await newRequest);
+    return fetch(newRequest);
   } else {
     newRequest = new Request(url, {
       method: request.method, // 指定使用原始请求的方法
